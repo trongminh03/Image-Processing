49f0b3f0f7850d1e0a2e77ea9a6c33ff4aaab5c1
Lê Trọng Minh <91895429+trongminh03@users.noreply.github.com>
2023-04-01 17:48:52 +0700

    update

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import glob
 from image_handler import convertImageMaker
 from image_generator import imageGeneratorOpenAI
 
-# imageGenerator = imageGeneratorOpenAI()
-# imageGenerator.generate_image("a pokemon picture", 1, '512x512', 'b64_json') 
-# imageGenerator.write_image()
+imageGenerator = imageGeneratorOpenAI()
+imageGenerator.generate_image("a pokemon picture", 1, '512x512', 'b64_json') 
+imageGenerator.write_image()
 
 files = glob.glob('./crop_images/*')
 for f in files:
     os.remove(f)
-
 
 convertTest = convertImageMaker() 
 convertTest.image_extract('original_image1') 
